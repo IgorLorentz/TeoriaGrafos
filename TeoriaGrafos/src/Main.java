9a5db323fdec67efac72b7c0b8755c6248e41399
Mannelz <emannuelm29@gmail.com>
2023-10-05 15:08:17 -0300

    main Project

--- TeoriaGrafos/src/Main.java
+++ TeoriaGrafos/src/Main.java
@@ -1,5 +1,3 @@
-package Euleriano;
-
 public class Main
 {
     public static void main(String[] args)
--- TeoriaGrafos/src/Prim/Main.java
+++
@@ -1,9 +0,0 @@
-package Prim;
-
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
-    }
-}
