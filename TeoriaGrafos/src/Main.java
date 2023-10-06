f5fd465547bce890747dc370f688d46ea1d039ab
Mannelz <emannuelm29@gmail.com>
2023-10-06 10:33:49 -0300

    create Grafo

--- TeoriaGrafos/src/Main.java
+++ TeoriaGrafos/src/Main.java
@@ -2,6 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        int[][] grafo = { {0, 1, 1, 1, 0, 0, 0,0}, {1, 0, 1, 0, 0, 0, 1, 0}, {1, 1, 0, 0, 1, 1, 1, 0},
+                {1, 0, 0, 0, 0, 1, 0, 1}, {0, 0, 1, 0, 0, 0, 1, 1}, {0, 0, 1, 1, 0, 0, 1, 1},
+                {0, 1, 1, 0, 1, 1, 0, 1}, {0, 0, 0, 1, 1, 1, 1, 0} };
+
+        for(int i = 0; i < grafo.length; i++)
+        {
+            for(int j = 0; j < grafo.length; j++)
+            {
+                System.out.print(grafo[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
