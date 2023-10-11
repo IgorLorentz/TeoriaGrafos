b82abfdeef00a948ed6f25906e2309b391816e0a
IgorLorentz <111509373+IgorLorentz@users.noreply.github.com>
2023-10-11 09:06:08 -0300

    add Dijkstra

--- TeoriaGrafos/src/Dijkstra.java
+++ TeoriaGrafos/src/Dijkstra.java
@@ -1,4 +1,37 @@
-public class Dijkstra
-{
-
-}
+//public class Dijkstra {
+//    int visitados [] = new int[7];
+//
+//    Cria conjunto de vertices Não_Visitados
+//
+//    for(int v : vertice){
+//        if(v != origem){
+//
+//        }
+//    }
+//    Para cada vértice v do grafo g{
+//        Se v!= origem{
+//            distancia[v] = INFINITO
+//        }
+//        anterior[v] = INDEFINIDO
+//        Insere v em Não_Visitados
+//    }
+//
+//    Enquanto Não_Visitados não for VAZIA{
+//        Procura vértice com menor distância, denominado u
+//
+//        Remove u de Não_Visitados
+//
+//        Para cada i vizinho de u{
+//            custo = distancia[u] + g[u, i]
+//
+//            Se custo < distancia[i] {
+//                distancia[i] = custo
+//                anterior[i] = u
+//            }
+//        }
+//    }
+//
+//        return distancia, anterior
+//}
+//
+//}
