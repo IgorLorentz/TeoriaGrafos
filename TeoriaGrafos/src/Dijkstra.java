11de9a879b4991eb665d42cd0a81a3aa8f9868f2
Mannelz <emannuelm29@gmail.com>
2023-11-10 20:33:14 -0300

    update dijkstra

--- TeoriaGrafos/src/Dijkstra.java
+++ TeoriaGrafos/src/Dijkstra.java
@@ -1,18 +1,14 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Dijkstra {
     // Aplica o algoritimo de Dijkstra
-    public void CalculateDijkstra(int [][] matriz, int initialPosition){
+    public void CalculateDijkstra(int [][] matriz, int initialVertice){
     int cost [] = new int[matriz.length];
     List <Caminho> border = new ArrayList<>();
     List <Caminho> ordenationBorder = new ArrayList<>();
     int antecessor [] = new int[matriz.length];
     int inclued [] = new int[matriz.length];
-    int position = 0;
-    Caminho c;
     //Inicializa variáveis
 
     for(int i=0; i<cost.length; i++){
@@ -21,21 +17,28 @@ public class Dijkstra {
     for(int i=0; i<antecessor.length; i++) {
         antecessor[i] = -1;
     }
-    cost[initialPosition] = 0;
-    antecessor [initialPosition] = -1;
+    cost[initialVertice] = 0;
+    antecessor [initialVertice] = initialVertice;
 
     //Loop da borda
-    int actualVertice = initialPosition;
+    int actualVertice = initialVertice;
     Caminho shortestPath;
     int size = 0;
     int menor = Integer.MAX_VALUE;
-    do{
 
+    do{
         for (int i = actualVertice; i == actualVertice; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                if (matriz[i][j] > 0) {
-                    ordenationBorder.add(c = new Caminho(actualVertice, j,matriz[i][j]));
-
+                if (matriz[i][j] > 0 && antecessor[j] == -1) {
+                    ordenationBorder.add(new Caminho(i, j, matriz[i][j]));
+                    cost[j] = cost[i] + matriz[i][j];
+                    antecessor[j] = i;
+                } else if (matriz[i][j] > 0 && antecessor[j] != -1) {
+                    if(cost[j] > matriz[i][j])
+                    {
+                        cost[j] = cost[i] + matriz[i][j];
+                        antecessor[j] = i;
+                    }
                 }
             }
         }
@@ -51,13 +54,19 @@ public class Dijkstra {
                 }
             }
         }
+
         border.addAll(ordenationBorder);
         ordenationBorder.clear();
+
+        /*
         //Adiciona custo
-        cost[border.get(0).destino] = border.get(0).peso;
+        //if(cost[border.get(0).destino] == Integer.MAX_VALUE)
+            cost[border.get(0).destino] = border.get(0).peso;
 
         //Adiciona o antecessor
-        antecessor[border.get(0).destino] = actualVertice;
+        //if(antecessor[border.get(0).destino] == -1)
+            antecessor[border.get(0).destino] = actualVertice;
+        */
         actualVertice = border.get(0).destino;
 
         //Adiciona incluidos e retira da borda
@@ -66,10 +75,28 @@ public class Dijkstra {
     }
     while(!border.isEmpty());
 
+    Imprimir(cost, antecessor);
 
+    /*
+    for(int i = 0; i == 0; i++)
+    {
+        System.out.print("Custos: ");
+        for(int j = 0; j < cost.length; j++)
+        {
+            System.out.print(cost[j] + " ");
+        }
 
+        System.out.println();
 
+        System.out.print("Antecessores: ");
+        for(int k = 0; k < antecessor.length; k++)
+        {
+            System.out.print(antecessor[k] + " ");
+        }
 
+        System.out.println();
+    }
+    /*
     //Impressões
         System.out.println("                            Vértices");
         System.out.println("              0  |  1  |  2  |  3  |  4  |  5  |  6  |");
@@ -86,6 +113,29 @@ public class Dijkstra {
         for(int i = 0; i < antecessor.length; i++)
         {
             System.out.printf("%d  |  ", antecessor[i]);
+        }
+     */
+    }
+
+    void Imprimir(int[] cost, int[] antecessor)
+    {
+        for(int i = 0; i == 0; i++)
+        {
+            System.out.print("Custos: ");
+            for(int j = 0; j < cost.length; j++)
+            {
+                System.out.print(cost[j] + " ");
+            }
+
+            System.out.println();
+
+            System.out.print("Antecessores: ");
+            for(int k = 0; k < antecessor.length; k++)
+            {
+                System.out.print(antecessor[k] + " ");
+            }
+
+            System.out.println();
         }
     }
 }
